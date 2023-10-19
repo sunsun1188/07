a930691448a929bdf0f8620b5d9b47e8fac54ead
sunsun1188 <eunsun08110811@gmail.com>
2023-10-19 16:04:49 +0900

    실습4

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int inc(int counter);
-int main(int argc, char *argv[]) {
 	
-	int i=10;
-	printf("함수 호출 전 i=%d\n",i);
-	inc(i);
-	// i=inc(i); 이렇게 하면 결과값 달라짐 
-	printf("함수 호출 후 i=%d\n",i);
+void f(void);
+
+int i;
+int main(void) {
+	for (i=0;i<5; i++) {
+		f();
+	}
 	return 0;
-}
+} 
 
-int inc(int counter) {
-	
-	counter++;
-	return counter;
-}
+void f(void) {
+	for (i=0;i<10;i++)
+		printf("#");
+} 
+// i를 전역변수가 아닌 지역변수로 설정하면 "*" 50개 찍힘 
