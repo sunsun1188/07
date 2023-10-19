9f0498dbce53b7ec20d7454520b73f016ffbafc5
sunsun1188 <eunsun08110811@gmail.com>
2023-10-19 15:46:19 +0900

    실습3

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int inc(int counter);
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-	for (i=0;i<5;i++) {
-		int temp=1;
-		printf("temp=%d\n", temp);
-		temp++;
-	}	
+	int i=10;
+	printf("함수 호출 전 i=%d\n",i);
+	inc(i);
+	// i=inc(i); 이렇게 하면 결과값 달라짐 
+	printf("함수 호출 후 i=%d\n",i);
 	return 0;
 }
-/*
-for (i=0;i<5;i++) {
-		
-		static int temp=1; 
-		printf("temp=%d\n", temp);
-		temp++;
-	}	*/ // temp=1,2,3,4,5 출력하고싶을 때의 코드  
+
+int inc(int counter) {
+	
+	counter++;
+	return counter;
+}
