006b1ceab9f32b0e49dcc52b4fc73304b780f2dc
sunsun1188 <eunsun08110811@gmail.com>
2023-10-19 15:19:56 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int flag=1;
+	int y;  // y 선언 해줘야함 
+	
+	while (flag!=0) {
+		int y;
+		y=3;
+		flag=0;
+	}
+	y=4;
+	return 0;
+}
