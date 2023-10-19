e2545d6302569ab21445010138d97cbacd4fbefe
sunsun1188 <eunsun08110811@gmail.com>
2023-10-19 16:20:53 +0900

    실습6-1

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-	
-void f(void);
 
-int i;
+int all_files;
+
+extern void sub();
+
 int main(void) {
-	for (i=0;i<5; i++) {
-		f();
-	}
+	sub();
+	printf("%d\n",all_files);
 	return 0;
-} 
+}
 
-void f(void) {
-	for (i=0;i<10;i++)
-		printf("#");
-} 
-// i를 전역변수가 아닌 지역변수로 설정하면 "*" 50개 찍힘 
